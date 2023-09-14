c1bab92c4391c12a76a1bec5dc2b19dff65f3420
MaiHoSyDat <maihosydat@gmail.com>
2023-09-14 17:16:50 +0700

    update Order

--- src/main/java/com/example/case6/service/IOrderService.java
+++ src/main/java/com/example/case6/service/IOrderService.java
@@ -5,6 +5,7 @@ import com.example.case6.model.Order;
 import java.util.List;
 
 public interface IOrderService {
+    Order update(Order order);
     Order save(Order order);
     List<Order> getByStatusAndCustomer(long statusId, long customerId);
     List<Order> getAllOrdersByCustomerId(long idCustomer);
