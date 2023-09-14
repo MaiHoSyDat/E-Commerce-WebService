e6b9f005b9d6a1e81ca36bf63b7d71721f4d557c
MaiHoSyDat <maihosydat@gmail.com>
2023-09-14 08:59:08 +0700

    fix model

--- src/main/java/com/example/case6/model/Code.java
+++ src/main/java/com/example/case6/model/Code.java
@@ -1,14 +1,15 @@
 package com.example.case6.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Code {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
@@ -16,4 +17,10 @@ public class Code {
     private String name;
     private int quantity;
     private double percent;
+    @ManyToOne
+    private Shop shop;
+
+    public Code(long id) {
+        this.id = id;
+    }
 }
