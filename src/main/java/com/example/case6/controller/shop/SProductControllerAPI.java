cafb04d545580fe6eda4aa2ad014491e7e572caf
MaiHoSyDat <maihosydat@gmail.com>
2023-09-15 12:51:33 +0700

    fix

--- src/main/java/com/example/case6/controller/shop/SProductControllerAPI.java
+++ src/main/java/com/example/case6/controller/shop/SProductControllerAPI.java
@@ -4,6 +4,7 @@ import com.example.case6.model.Image;
 import com.example.case6.model.Product;
 import com.example.case6.model.Shop;
 import com.example.case6.model.dto.ProductDTO;
+import com.example.case6.repository.IImageRepo;
 import com.example.case6.service.IProductService;
 import com.example.case6.service.IShopService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,25 +22,30 @@ public class SProductControllerAPI {
     private IProductService productService;
     @Autowired
     private IShopService shopService;
+    @Autowired
+    private IImageRepo iImageRepo;
 
     @GetMapping("/{idShop}/products")
     public ResponseEntity<List<Product>> getProductByShopId(@PathVariable long idShop) {
         return new ResponseEntity<>(productService.getProductByShopId(idShop), HttpStatus.OK);
     }
+
     //14
     @PostMapping("/{idShop}/products/create")
     public ResponseEntity<ProductDTO> saveProductByShopId(@PathVariable long idShop, @RequestBody ProductDTO productDTO) {
 
-        productService.save(productDTO , idShop);
+        productService.save(productDTO, idShop);
 
         return new ResponseEntity<>(productDTO, HttpStatus.OK);
     }
+
     //15, 16, 17, 18
     @PostMapping("/{idShop}/products/{idProduct}")
     public ResponseEntity<Product> updateProductByShopId(@PathVariable long idShop, @PathVariable long idProduct, @RequestBody Product product) {
         productService.edit(product);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
+
     //19
     @GetMapping("/{idShop}/products/{idProduct}")
     public ResponseEntity<Product> findProductByShopId(@PathVariable long idShop, @PathVariable long idProduct) {
@@ -47,7 +53,10 @@ public class SProductControllerAPI {
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
-
+    @GetMapping("/getImages/{productId}")
+    public ResponseEntity<List<Image>> getImageByProductId(@PathVariable long productId) {
+        return new ResponseEntity<>(iImageRepo.findAllByProductId(productId), HttpStatus.OK);
+    }
 
 
 }
